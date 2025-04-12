8b32f91a8ff016bd2fc66d483b11305f84db8d66
ns loni <nsloni@gmail.com>
2025-04-12 18:43:48 +0530

    updated

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+REPO_NAME = "Mongo_Python_Package_MLOPs"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "nsgowebjavaprog"
+AUTHOR_EMAIL = "nsgowebjavaprog209@gmail.com"
 
 setup(
     name=PKG_NAME,
